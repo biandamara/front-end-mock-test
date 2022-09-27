8f7c652934a320def1028f185aac022a7c8e52ba
Bian Damara <bian.damara@gmail.com>
2022-09-27 18:29:29 +0700

    edit login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,3 +1,6 @@
+// import { Axios } from "axios";
+// import { useEffect } from "react";
+
 // import components
 import Navigation from "../component/Navigation";
 
@@ -5,12 +8,42 @@ import Navigation from "../component/Navigation";
 import styles from "../assets/styles/Login.module.css";
 
 function Login() {
+  // useEffect(() => {
+  //   Axios.get("https://test-binar.herokuapp.com")
+  //     .then((res) => console.log(res.data))
+  //     .catch((err) => console.log(err));
+  // }, []);
+
   return (
     <>
       <Navigation />
       {/* container login page - start */}
       <div className={`container ${styles.page}`}>
-        <h1>Login</h1>
+        <div className={`text-center`}>
+          {/* title - start */}
+          <h1>Login</h1>
+          {/* title - end */}
+
+          {/* form login - start */}
+          <div className={`${styles.box}`}>
+            <div className={`${styles.form}`}>
+              <form>
+                <input placeholder="E-mail" type="email" />
+              </form>
+              <form>
+                <input placeholder="Password" type="password" />
+              </form>
+              <button>Login</button>
+            </div>
+          </div>
+          {/* form login - end */}
+
+          {/* to register - start */}
+          <p>
+            Don't have an account? <a href="/Register">Register</a>
+          </p>
+          {/* to register - end */}
+        </div>
       </div>
       {/* container login page - end */}
     </>
